3c3f40cea5002cc910414ffc209dd4353b6fb57f
JDarkGreen <jgomez.4net@gmail.com>
2016-12-30 18:09:38 -0500

    actualizacion de contacto

--- themes/capitalaudittheme/email/enviar.php
+++ themes/capitalaudittheme/email/enviar.php
@@ -9,32 +9,27 @@
 
 	//Email A quien se le rinde cuentas
 	$webmaster_email  = "jgonzales@capitalaudit.com.pe";
-	$webmaster_email2 = "jgomez@ingenioart.com";
+	$webmaster_email2 = "jesusmgonzalesaguilar@gmail.com";
+	$webmaster_email3 = "gonzalesgonzalesjose224@gmail.com";
+	$webmaster_email4 = "webmaster@ingenioart.com";
 
 	include("./class.phpmailer.php");
  	include("./class.smtp.php");
 
 	$mail = new PHPMailer();
 
-	/* $mail->IsSMTP(); // send via SMTP*
-	$mail->SMTPSecure = 'ssl'; 
-
-	/*$mail->Host      = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-	$mail->Port      = 465;
-	$mail->SMTPAuth  = true; // turn on SMTP authentication
-	$mail->Username  = "jgomez.4net@gmail.com"; // Enter your SMTP username
-	$mail->Password  = "ARLAC_RINO1EVER"; // SMTP password*/
-
 	$mail->From     = $from;
 	$mail->FromName = $name;
 	$mail->AddAddress( $webmaster_email );
 	$mail->AddAddress( $webmaster_email2 );
+	$mail->AddAddress( $webmaster_email3 );
+	$mail->AddAddress( $webmaster_email4 );
 
 	$mail->IsHTML(false); // send as HTML
 	$mail->Subject = $subject;
 
 	//Customizar el mensaje
-	$message .= " Telefono / Celular de Contacto: " . $tel;
+	$message .= " Telefono: " . $tel;
 
 	$mail->Body = $message;
 
